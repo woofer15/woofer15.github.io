e2ac891749af4807bb74a27b5380b85c090c089e
ssandhu403 <101891570+ssandhu403@users.noreply.github.com>
2022-06-01 16:15:22 -0600

    Add files via upload

--- multiplication/multiplication.js
+++ multiplication/multiplication.js
@@ -1,40 +1,5 @@
-let possibleQuestions = [];
-let myQuestions = [];
-
-function clearStorage() {
-    localStorage.clear();
-    console.log('clear');
-};
-
-function start() {
-    possibleQuestions = []
-    myQuestions = []
-    
-    let questionResultFromStorage
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            questionResultFromStorage = localStorage.getItem(i + 'x' + j)
-            questionResultFromStorage = JSON.parse(questionResultFromStorage)
-            createStorage(i, j);
-        }
-    }
-
-    findQuestions(questionResultFromStorage);
-    let questionIndex = []
-    let index
-    while (questionIndex.length < 10) {
-        index = getRandomIntBetween(0, possibleQuestions.length - 1)
-        if (questionIndex.indexOf(index) === -1) {
-            questionIndex.push(index)
-        }
-    }
-    for (let x = 0; x < 10; x++) {
-        myQuestions.push(possibleQuestions[questionIndex[x]]);
-    };
-    console.log(myQuestions);
-}
-
-
+let possibleQuestions = []
+let myQuestions = []
 let entry = {
     firstTry : [
         false, //question done
@@ -51,77 +16,97 @@ let entry = {
         false, //question correct
         0 //number of milliseconds
     ]
+}
+
+function clearStorage() {
+    localStorage.clear();
+    console.log('clear');
 };
 
+function start() {
+    
+    // will create default entry in storage for all items that are not found
+    for (let i = 1; i < 13; i++) {
+        for (let j = 1; j < 13; j++) {
+            createStorage(i, j)
+        }
+    }
+
+    // fill possibleQuestions array
+    findQuestions()
+
+    // get 10 random possibleQuestions and put into myQuestions
+    let questionIndex = []
+    let index
+    while (questionIndex.length < 10) {
+        index = getRandomIntBetween(0, possibleQuestions.length - 1)
+        if (questionIndex.indexOf(index) === -1) {
+            questionIndex.push(index)
+        }
+    }
+    for (let x = 0; x < 10; x++) {
+        myQuestions.push(possibleQuestions[questionIndex[x]]);
+    }
+
+    console.log(myQuestions);
+
+    displayQuestion(0)
+    document.getElementById('questionAndAnswerContainer').style.display = 'block'
+}
+
+function displayQuestion(index) {
+    document.getElementById('question').innerText = myQuestions[index] + " = "
+}
+
+function createKey(first, second) {
+    return first + 'x' +second
+}
+
 function createStorage(first, second) {
-    let key = first + 'x' + second
+    let key = createKey(first, second)
  
     if (localStorage.getItem(key) === null) {
         localStorage.setItem(key, JSON.stringify(entry))
     }
-
-    readStorage(first, second)
 }
 
 function readStorage(first, second) {
-    let questionResultFromStorage = localStorage.getItem(first + 'x' + second)
-    questionResultFromStorage = JSON.parse(questionResultFromStorage)
+    let questionResultFromStorage = localStorage.getItem(createKey(first, second))
+    if (questionResultFromStorage === undefined) {
+        return undefined
+    }
+    else {
+        return JSON.parse(questionResultFromStorage)
+    }
 }
 
-function findQuestions(questionStorage, first, second) {
+function findQuestions() {
     for (let i = 1; i < 13; i++) {
         for (let j = 1; j < 13; j++) {
-            if (questionStorage.firstTry[0] === false) {
-                possibleQuestions.push(i + 'x' + j)
+            let questionFromStorage = readStorage(i, j)
+            if (questionFromStorage.firstTry[0] === false) {
+                possibleQuestions.push(createKey(i, j))
             }
         }
     }
 
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            if (possibleQuestions.length < 10) {
-                if (questionStorage.secondTry[0] === false) {
-                    possibleQuestions.push(i + 'x' + j)
+    if (possibleQuestions.length < 10) {
+        for (let i = 1; i < 13; i++) {
+            for (let j = 1; j < 13; j++) {
+                let questionFromStorage = readStorage(i, j)
+                if (questionFromStorage.secondTry[0] === false) {
+                    possibleQuestions.push(createKey(i, j))
                 }
             }
         }
     }
 
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            if (possibleQuestions.length < 10) {
-                if (questionStorage.thirdTry[0] === false) {
-                    possibleQuestions.push(i + 'x' + j)
-                }
-            }
-        }
-    }
-
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            if (possibleQuestions.length < 10) {
-                if (questionStorage.firstTry[1] === false) {
-                    possibleQuestions.push(i + 'x' + j)
-                }
-            }
-        }
-    }
-
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            if (possibleQuestions.length < 10) {
-                if (questionStorage.secondTry[1] === false) {
-                    possibleQuestions.push(i + 'x' + j)
-                }
-            }
-        }
-    }
-
-    for (let i = 1; i < 13; i++) {
-        for (let j = 1; j < 13; j++) {
-            if (possibleQuestions.length < 10) {
-                if (questionStorage.thirdTry[1] === false) {
-                    possibleQuestions.push(i + 'x' + j)
+    if (possibleQuestions.length < 10) {
+        for (let i = 1; i < 13; i++) {
+            for (let j = 1; j < 13; j++) {
+                let questionFromStorage = readStorage(i, j)
+                if (questionFromStorage.thirdTry[0] === false) {
+                    possibleQuestions.push(createKey(i, j))
                 }
             }
         }
